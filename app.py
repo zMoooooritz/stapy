067c77a1eebda9cc9ccacf87a019546882ba86b4
Moritz Biering <moritzbiering.mb@gmail.com>
2021-08-23 20:50:08 +0200

    Add basic support to store config parameters in config.ini

--- app.py
+++ app.py
@@ -4,20 +4,37 @@ from sta.entity import Entity
 from sta.post import Post
 from sta.query import Query, Expand
 from common.log import Log, custom_logger
+from common.config import Config
 
 import argparse
 
 logger = None
 
+# TODO implement add / del / get in a basic way
+# TODO add documentation (and readme)
+# TODO add more tests and make them run properly
+# TODO allow for patch / update
+# TODO add CLI - Dialogs for add / del / get
+
 def run():
+
+    config = Config()
 
     parser = argparse.ArgumentParser(
         description="Access and modify data that is stored in a server that uses the SensorThings API (v1.1+)",
         prog="STApy", epilog="")
-    parser.add_argument("-d", "--del", "--delete", nargs="+", dest="delete", metavar=("Entity", "ID/Path"),
-                        help="delete entities by id or path")
     parser.add_argument("-l", "--log", type=Log.from_string, choices=list(Log), default=Log.INFO,
                         help="define the log level", metavar="CRITICAL,ERROR,WARNING,INFO,DEBUG,NOTSET")
+    parser.add_argument("-u", "--url-set", dest="urlset", nargs=1, metavar=("URL"),
+                        help="set the url of the SensorThings API backend")
+    parser.add_argument("-ug", "--url-get", dest="urlget", action="store_true",
+                        help="get the url of the SensorThings API backend")
+    parser.add_argument("-a", "--add", nargs="+", metavar=("Entity", "Parameters"),
+                        help="add new entities")
+    parser.add_argument("-d", "--del", nargs="+", dest="delete", metavar=("Entity", "ID/Path"),
+                        help="delete entities by id or path")
+    parser.add_argument("-g", "--get", nargs="+", dest="getr", metavar=("Entity", "ID/Path"),
+                        help="get the content of entities by id or path")
 
     args = parser.parse_args()
 
@@ -25,8 +42,28 @@ def run():
     logger = custom_logger('root', args.log.value)
     logger.info("starting application")
 
+    if args.urlset:
+        url = args.urlset
+        if isinstance(args.urlset, list):
+            url = args.urlset[0]
+        url = str(url)
+        if not url.endswith("/"):
+            url = url + "/"
+        config.set(API_URL = url)
+    if args.urlget:
+        print("The currently set API_URL is: " + str(config.get("API_URL")))
+
+    if (args.add or args.delete or args.getr) and config.get("API_URL") == "":
+        logger.critical("The url has to be set before using the application (see --help)")
+        logger.info("ending application")
+        return True
+
+
+
     # Add requests here
-    Post().new_location("Test-Location", "Test-Location-Description", "Lul", [0])
+    # Post().new_location("Test-Location", "Test-Location-Description", "Lul", [0])
+
+    config.save()
 
     logger.info("ending application")
     return True
---
+++ common/config.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import logging
+import sys
+import configparser
+
+logger = logging.getLogger('root')
+
+class Config:
+
+    FILENAME = "config.ini"
+
+    def __init__(self):
+        self.config = configparser.ConfigParser()
+        self.read()
+
+    def read(self):
+        try:
+            self.config.read(self.FILENAME)
+        except:
+            logger.critical("Config file does not exist")
+            logger.info("ending application")
+            sys.exit()
+
+    def save(self):
+        try:
+            with open(self.FILENAME, 'w') as configfile:
+                self.config.write(configfile)
+        except:
+            logger.critical("Writing the config file did fail")
+            logger.info("ending application")
+            sys.exit()
+
+    def get(self, arg):
+        try:
+            return self.config["DEFAULT"][arg]
+        except:
+            logger.critical("The provided key (" + str(arg) + ") does not exist in the config file")
+            logger.info("ending application")
+            sys.exit()
+
+    def set(self, **kwargs):
+        for k,v in kwargs.items():
+            self.config["DEFAULT"][k] = str(v)
