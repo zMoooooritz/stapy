5768b8690078adabb1be1342fbacf21a9bd36fcf
Moritz Biering <moritzbiering.mb@gmail.com>
2022-03-10 15:01:36 +0100

    Fix parsing of dates

--- tests/test_time.py
+++ tests/test_time.py
@@ -5,17 +5,18 @@ from stapy.sta.time import Time
 class TestTimeMethods(unittest.TestCase):
 
     def setUp(self):
-        self.str1 = "22:22:22 01.01.2020" 
-        self.str2 = "01.01.2010 22:22:22" 
+        self.str1 = "2020-03-31 12:34:56"
+        self.str2 = "12:34:56 2020-12-24"
 
     def test_time(self):
         with self.assertRaises(Exception):
             Time(123)
-        self.assertEqual(str(Time(self.str1)), "2020-01-01T22:22:22")
-        self.assertEqual(str(Time(self.str1 + "-" + self.str2)), "2010-01-01T22:22:22-2020-01-01T22:22:22")
-        self.assertEqual(str(Time(self.str1 + "-" + self.str2)), str(Time(self.str2 + "-" + self.str1)))
-        self.assertEqual(str(Time("-" + self.str1)), "2020-01-01T22:22:22")
-        self.assertEqual(str(Time(self.str1 + "-" + self.str1)), "2020-01-01T22:22:22")
+        self.assertEqual(str(Time(self.str1)), "2020-03-31T12:34:56")
+        self.assertEqual(str(Time(self.str1 + "/" + self.str2)), "2020-03-31T12:34:56/2020-12-24T12:34:56")
+        self.assertEqual(str(Time(self.str1 + "/" + self.str2)), str(Time(self.str2 + "/" + self.str1)))
+        self.assertEqual(str(Time("/" + self.str1)), "2020-03-31T12:34:56")
+        self.assertEqual(str(Time(self.str1 + "/" + self.str1)), "2020-03-31T12:34:56")
+        self.assertEqual(str(Time("31.03.2020 12:34:56")), "2020-03-31T12:34:56")
 
 
 if __name__ == "__main__":
